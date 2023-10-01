38b9604520810de1c41a5c3b1645e890b12aa148
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-10-01 13:27:51 +0300

    2nd fetch

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ axios.defaults.headers.common['x-api-key'] =
 const refs = {
   select: document.querySelector('.breed-select'),
 };
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+
+fetchBreeds()
+  .then(data => {
+    refs.select.innerHTML = createList(data);
+  })
+  .catch(err => console.log(err));
+
+fetchCatByBreed('abys')
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => console.log(err));
 
 function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds').then(resp => {
+  return fetch(`${BASE_URL}breeds`).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.status);
     }
@@ -16,11 +29,14 @@ function fetchBreeds() {
   });
 }
 
-fetchBreeds()
-  .then(data => {
-    refs.select.innerHTML = createList(data);
-  })
-  .catch(err => console.log(err));
+function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_URL}images/search?breed_ids=${breedId}`).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
+    }
+    return resp.json();
+  });
+}
 
 function createList(arr) {
   return arr
